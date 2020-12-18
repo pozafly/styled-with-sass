00537f3df2c828c63c110531db20ae432e8bad19
HwangSunTae <pozafly@gmail.com>
2020-12-18 12:27:29 +0900

    styled component 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import SassComponent from './SassComponent';
+import StyledComponents from './StyledComponents';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <StyledComponents />
       </div>
     )
   }
